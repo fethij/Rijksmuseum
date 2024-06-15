5b64fbc1b1123b7e9db41b11720b659a6004a30f
Fethi Tewelde <fethi.tewelde@politie.nl>
2024-06-15 04:13:11 +0200

    android version bump

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -105,8 +105,8 @@ android {
         applicationId = "com.tewelde.rijksmuseum"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
     packaging {
         resources {
