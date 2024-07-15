00445ed468ffaaa813377142c848cfa22fb65e60
Fethi Tewelde <fethi.tewelde@politie.nl>
2024-07-16 01:40:45 +0200

    change back button to Ios version

--- feature/arts/src/commonMain/kotlin/com/tewelde/rijksmuseum/feature/arts/detail/DetailScreen.kt
+++ feature/arts/src/commonMain/kotlin/com/tewelde/rijksmuseum/feature/arts/detail/DetailScreen.kt
@@ -12,7 +12,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.outlined.ArrowBackIos
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -80,7 +80,7 @@ fun DetailScreen(
                         onClick = { onBackClick() },
                     ) {
                         Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            imageVector = Icons.AutoMirrored.Outlined.ArrowBackIos,
                             contentDescription = null
                         )
                     }
