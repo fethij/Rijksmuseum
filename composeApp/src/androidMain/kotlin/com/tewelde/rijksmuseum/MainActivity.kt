5c79edaa0209da4c198984ea89042e1a32f06a8a
fethij <>
2025-11-04 09:14:33 +0100

    add EdgeToEdgeSideEffect composable for dynamic system bar styling

--- composeApp/src/androidMain/kotlin/com/tewelde/rijksmuseum/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/tewelde/rijksmuseum/MainActivity.kt
@@ -4,9 +4,12 @@ import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
+import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 
@@ -20,9 +23,53 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             App()
+            EdgeToEdgeSideEffect(
+                isStatusBarLight = !isSystemInDarkTheme(),
+                isNavigationBarLight = !isSystemInDarkTheme()
+            )
         }
     }
 }
+
+@Composable
+fun EdgeToEdgeSideEffect(
+    isStatusBarLight: Boolean,
+    isNavigationBarLight: Boolean,
+) {
+    val activity = LocalActivity.current as ComponentActivity
+
+    DisposableEffect(
+        isStatusBarLight,
+        isNavigationBarLight
+    ) {
+        activity.enableEdgeToEdge(
+            statusBarStyle =
+                if (isStatusBarLight) {
+                    SystemBarStyle.light(
+                        Color.TRANSPARENT,
+                        Color.TRANSPARENT
+                    )
+                } else {
+                    SystemBarStyle.dark(
+                        Color.TRANSPARENT
+                    )
+                },
+            navigationBarStyle =
+                if (isNavigationBarLight) {
+                    SystemBarStyle.light(
+                        Color.TRANSPARENT,
+                        Color.TRANSPARENT
+                    )
+                } else {
+                    SystemBarStyle.dark(
+                        Color.TRANSPARENT
+                    )
+                }
+        )
+        onDispose { }
+    }
+}
+
 
 @Preview
 @Composable
